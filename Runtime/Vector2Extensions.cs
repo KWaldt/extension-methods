794226e0a8de4c864992b0e361c529bfbb9912b6
Kristina Waldt <waldt.kristina@gmail.com>
2025-12-04 22:11:17 +0100

    Added: Vector2Int conversions

---
+++ Runtime/Vector2Extensions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace KristinaWaldt.ExtensionMethods
+{
+    public static class Vector2Extensions
+    {
+        /// <summary>
+        /// Rounds the xy and returns them as a Vector2Int.
+        /// z is discarded.
+        /// </summary>
+        public static Vector2Int ToVector2Int(this Vector3 original)
+        {
+            return new Vector2Int(Mathf.RoundToInt(original.x), Mathf.RoundToInt(original.y));
+        }
+        
+        /// <summary>
+        /// Rounds the xy and returns them as a Vector2Int.
+        /// </summary>
+        public static Vector2Int ToVector2Int(this Vector2 original)
+        {
+            return new Vector2Int(Mathf.RoundToInt(original.x), Mathf.RoundToInt(original.y));
+        }
+    }
+}
